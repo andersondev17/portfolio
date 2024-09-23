0f95c0ceaf9393e11e8480c036bb9f1f6f12fca6
Anderson Lopez <52222621+TheGodFatherpte@users.noreply.github.com>
2024-09-23 11:48:28 -0500

    Add hire me and downloadCV link

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import MagicButton from "@/components/ui/MagicButton"; // Import the MagicButton component
 import { navItems } from "@/data";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { FaBriefcase } from "react-icons/fa"; // You might need to install react-icons
+
 export default function Home() {
 
   const { scrollYProgress } = useScroll();
@@ -20,63 +23,73 @@ export default function Home() {
   return (
 
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 perspective-lg">
-    <div className="max-w-7xl w-full">
-      <FloatingNav navItems={navItems} />
+      <div className="fixed top-4 right-4 z-50"> {/* Fixed positioning */}
+        <a href="mailto:anderson.dev17@gmail.com">
+          <MagicButton
+            title="Hire Me"
+            icon={<FaBriefcase />}
+            position="right"
+            otherclasses="!bg-purple-500 hover:!bg-green-700"
+          />
+        </a>
+      </div>
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
 
-      <motion.div
-        style={{ scale, rotateX }}
-        initial={{ opacity: 0, y: 50, rotateX: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.1, ease: "easeOut", delay: 0.3 }}
-      >
-        <Hero />
-      </motion.div>
+        <motion.div
+          style={{ scale, rotateX }}
+          initial={{ opacity: 0, y: 50, rotateX: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.1, ease: "easeOut", delay: 0.3 }}
+        >
+          <Hero />
+        </motion.div>
 
-      <motion.div
-        style={{ scale }}
-        initial={{ opacity: 0, x: -30, scale: 0.8 }}
-        whileInView={{
-          opacity: 1,
-          x: 0,
-          scale: 1,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-        }}
-        transition={{
-          duration: 1,
-          ease: [0.25, 0.46, 0.45, 0.94],
-          delay: 0.2,
-        }}
-      >
-        <Grid /> {/* Grid dinámico */}
-      </motion.div>
+        <motion.div
+          style={{ scale }}
+          initial={{ opacity: 0, x: -30, scale: 0.8 }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+            scale: 1,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+          }}
+          transition={{
+            duration: 1,
+            ease: [0.25, 0.46, 0.45, 0.94],
+            delay: 0.2,
+          }}
+        >
+          <Grid /> {/* Grid dinámico */}
+        </motion.div>
 
-      <motion.div
-        style={{ scale }}
-        initial={{ opacity: 0, x: 50, rotateY: 20 }}
-        whileInView={{ opacity: 1, x: 0, rotateY: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut", delay: 0.6 }}
-      >
-        <RecentProjects />
-      </motion.div>
+        <motion.div
+          style={{ scale }}
+          initial={{ opacity: 0, x: 50, rotateY: 20 }}
+          whileInView={{ opacity: 1, x: 0, rotateY: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.6 }}
+        >
+          <RecentProjects />
+        </motion.div>
 
-      <motion.div
-        style={{ scale }}
-        initial={{ opacity: 0, scale: 0.8, z: -10 }}
-        whileInView={{ opacity: 1, scale: 1, z: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
-      >
-        <Experience />
-      </motion.div>
-      <motion.div
-        style={{ scale }}
-        initial={{ opacity: 0, scale: 0.8, z: -10 }}
-        whileInView={{ opacity: 1, scale: 1, z: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
-      >
-        <Approach />
-      </motion.div>
-      <Footer />
-    </div>
-  </main>
+        <motion.div
+          style={{ scale }}
+          initial={{ opacity: 0, scale: 0.8, z: -10 }}
+          whileInView={{ opacity: 1, scale: 1, z: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
+        >
+          <Experience />
+        </motion.div>
+        <motion.div
+          style={{ scale }}
+          initial={{ opacity: 0, scale: 0.8, z: -10 }}
+          whileInView={{ opacity: 1, scale: 1, z: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
+        >
+          <Approach />
+        </motion.div>
+        <Footer />
+      </div>
+    </main>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,64 +1,82 @@
-import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
+import { FaFileDownload, FaLocationArrow } from "react-icons/fa";
 import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
-    return (
-        <footer className="w-full pt-20 pb-10" id="contact">
-            {/* background grid */}
-            <div className="w-full absolute left-0 -bottom-72 min-h-96">
-                <img
-                    src="/footer-grid.svg"
-                    alt="grid"
-                    className="w-full h-full opacity-50 "
-                />
-            </div>
+  const handleDownloadCV = () => {
+    const cvLink =
+      "https://raw.githubusercontent.com/TheGodFatherpte/portfolio/master/AndersonLopezCV.pdf"; // Reemplaza con tu URL directa
+    window.open(cvLink, "_blank"); // Abre el CV en una nueva pestaña
+  };
 
-            <div className="flex flex-col items-center">
-                <h1 className="heading lg:max-w-[45vw]">
-                    Ready to take <span className="text-purple">your</span> digital
-                    presence to the next level?
-                </h1>
-                <p className="text-white-200 md:mt-10 my-5 text-center">
-                    Reach out to me today and let&apos;s discuss how I can help you
-                    achieve your goals.
-                </p>
-                <a href="mailto:anderson.dev17@gmail.com">
-                    <MagicButton
-                        title="Let's get in touch"
-                        icon={<FaLocationArrow />}
-                        position="right"
-                    />
-                </a>
-            </div>
-            <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-                <p className="md:text-base text-sm md:font-normal font-light">
-                    Copyright © 2024 Anderson Lopez
-                </p>
-                <br />
-                <div className="flex items-center md:gap-3 gap-6">
-                    {socialMedia.map((info) => (
-                        <div
-                            key={info.id}
-                            className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-                        >
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      {/* background grid */}
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50 "
+        />
+      </div>
 
-                            <a
-                                key={info.id}
-                                href={info.link}
-                                target="_blank" // Opcional: abre el enlace en una nueva pestaña
-                                rel="noopener noreferrer" // Opcional: mejora la seguridad al abrir enlaces externos
-                                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-                            >
-                                <img src={info.img} alt="icons" width={20} height={20} />
-                            </a>
-                        </div>
-                    ))}
-                </div>
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple">your</span> digital presence
+          to the next level?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let's discuss how I can help you achieve your
+          goals.
+        </p>
+
+        {/* Buttons container */}
+        <div className="flex flex-col md:flex-row items-center gap-4"> {/* Add flex container */}
+          <a href="mailto:anderson.dev17@gmail.com">
+            <MagicButton
+              title="Let's get in touch"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+
+          {/* CV download button */}
+          <MagicButton
+            title="Download CV"
+            icon={<FaFileDownload />}
+            position="right"
+            handleclick={handleDownloadCV}
+            otherclasses="!bg-[#161A31]"
+          />
+        </div>
+      </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Anderson Lopez
+        </p>
+        <br />
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <div
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <a
+                key={info.id}
+                href={info.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img src={info.img} alt="icons" width={20} height={20} />
+              </a>
             </div>
-        </footer>
-    );
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
